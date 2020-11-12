90ab4e9ec5a97e480fb2fe02fba7423bdee7ca1b
Teslak <monolit_rex@mail.ru>
2020-11-12 11:48:06 +0300

    ку
    аы

---
+++ copy.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    let copy = document.getElementById("copy");
+
+    copy.onclick = function() {
+        const el = document.createElement('textarea');
+        el.value = copy.dataset.text;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        const selected =
+        document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        if (selected) {
+            document.getSelection().removeAllRanges();
+            document.getSelection().addRange(selected);
+        }
+    }
+}
